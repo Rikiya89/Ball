cbbdadfec44f86d58bd9f88bdd0eb67fa50658bb
Rikiya89 <83910358+Rikiya89@users.noreply.github.com>
2023-01-08 19:49:36 +0900

    Update Ball.cpp

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -7,7 +7,35 @@
 
 #include "Ball.h"
 
+Ball::Ball(){
+    pos = glm::vec2(400,300);// setting for position
+    radius = 80.0; //setting for radius
+}
+
 void Ball::draw(){
+    ofSetColor(31, 63, 255,100);
+    ofDrawCircle(pos.x, pos.y, radius);//draw two circle
+    ofSetColor(255, 0, 0, 200);
+    ofDrawCircle(pos.x, pos.y, radius/10.0);
     ofSetColor(31, 63, 255);
-    ofDrawCircle(ofGetWidth()/2, ofGetHeight()/2,100);//draw a circle in the center
+}
+
+//pos setter
+void Ball::setPos(glm::vec2 _pos){
+    pos = _pos;
+}
+
+//pos getter
+glm::vec2 Ball::getPos() {
+    return pos;
+}
+
+//radius setter
+void Ball::setRadius(float _radius) {
+    radius = _radius;
+}
+
+//radius getter
+float Ball::getRadius(){
+    return radius;
 }
