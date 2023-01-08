7fc0669764e93a167f353464995e13e61c7c1fb2
Rikiya89 <83910358+Rikiya89@users.noreply.github.com>
2023-01-08 19:49:34 +0900

    Update Ball.h

--- src/Ball.h
+++ src/Ball.h
@@ -1,5 +1,5 @@
 //
-//  Ball.hpp
+//  Ball.h
 //  Ball
 //
 //  Created by Ricky O'kawa on 2023/01/08.
@@ -8,9 +8,18 @@
 #include "ofMain.h"
 
 class Ball {
+private:
+ glm::vec2 pos; //positon for a circle
+    float radius; // radius for a circle
+
+
 public:
-    //ini method
+    Ball(); //constructor
     void draw(); //draw a circle
+    void setRadius(float radius);//radius setter
+    float getRadius();//radius getter
+    void setPos(glm::vec2 pos);//pos setter
+    glm::vec2 getPos(); //pos getter
 };
 
 #ifndef Ball_h
