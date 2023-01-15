bf077dc2d40c758609cde4acf10cc32ff582cd78
Rikiya89 <83910358+Rikiya89@users.noreply.github.com>
2023-01-16 00:23:27 +0900

    Update Ball.h

--- src/Ball.h
+++ src/Ball.h
@@ -8,18 +8,25 @@
 #include "ofMain.h"
 
 class Ball {
+
 private:
- glm::vec2 pos; //positon for a circle
+    glm::vec2 pos; //positon for a circle
     float radius; // radius for a circle
-
-
+    glm::vec2 speed; //circle of speed
+    float phase; //reducing a circle
+    float phaseSpeed;//reducing speed
+    float strechedRadius; //reducing radius
+    
 public:
     Ball(); //constructor
     void draw(); //draw a circle
+    void update();
     void setRadius(float radius);//radius setter
     float getRadius();//radius getter
     void setPos(glm::vec2 pos);//pos setter
     glm::vec2 getPos(); //pos getter
+    void setSpeed(glm::vec2 speed); //speed setter
+    glm::vec2 getSpeed();//speed getter
 };
 
 #ifndef Ball_h
